462947f8807f308e3e8114280f03352945de33bd
VANDIT TYAGI <91458535+vandit98@users.noreply.github.com>
2023-05-23 19:03:26 +0530

    Time: 129 ms (89.36%), Space: 18.9 MB (37.39%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,14 +2,19 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
 #        we can use the property of xor for a number i.e when we take xor with itself it returns zero
-        nums.sort()
-        i=0
-        while(i<len(nums)-1):
-            if nums[i]^nums[i+1]==0:
-                i+=2
-            else:
-                return nums[i]
-        return nums[-1]
+        # approach-1 time compexity-O(nlog(n))
+        # nums.sort()
+        # i=0
+        # while(i<len(nums)-1):
+        #     if nums[i]^nums[i+1]==0:
+        #         i+=2
+        #     else:
+        #         return nums[i]
+        # return nums[-1]
+        ans=0
+        for i in nums:
+            ans=ans^i
+        return ans
             
             
             
