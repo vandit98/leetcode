ff9392807dacfa06bd3547180eca8f71d6086a01
VANDIT TYAGI <91458535+vandit98@users.noreply.github.com>
2023-05-24 12:53:06 +0530

    Time: 61 ms (5.19%), Space: 16.2 MB (30.80%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,21 +1,50 @@
 class Solution:
-    def Binary_to_int(self,bi_no)->int:
-        no=0
-        for i in range(len(bi_no)):
-            if bi_no[len(bi_no)-i-1]=='1':
-                no+=pow(2,i)
-        return no
+    # def Binary_to_int(self,bi_no)->int:
+    #     no=0
+    #     for i in range(len(bi_no)):
+    #         if bi_no[len(bi_no)-i-1]=='1':
+    #             no+=pow(2,i)
+    #     return no
     def numSteps(self, s: str) -> int:
-        num=self.Binary_to_int(s)
-        # print("returned no is",num)
-        count=0
-        while(num!=1):
-            if num%2!=0:
-                num+=1
-                # print(num)
-            else:
-                num=num//2
-                # print(num)
-            count+=1
-        return count
+    #     num=self.Binary_to_int(s)
+    #     # print("returned no is",num)
+    #     count=0
+    #     while(num!=1):
+    #         if num%2!=0:
+    #             num+=1
+    #             # print(num)
+    #         else:
+    #             num=num//2
+    #             # print(num)
+    #         count+=1
+    #     return count
+    
+    
+#     solution-2 O(n)
+#     initially no of operation for 0 and 1
+        dictt={"0":1,"1":2}
+        first_one=False
+        ans=0
+        for j in range(len(s)-1):
+            i=s[len(s)-j-1]
+            print("we got i as",i)
+            if i=="1" and first_one==False:
+                first_one=True
+                dictt["0"]=2
+                dictt["1"]=1
+                ans+=2
+                print("changing the dicctt pattern")
+                print("ans is",ans)
+                continue
+            ans+=dictt[i]
+            print("ans is",ans)
+        print(ans)
+        if first_one==True:
+            ans+=1
+            
+        return ans
+            
+            
+            
+        
         
