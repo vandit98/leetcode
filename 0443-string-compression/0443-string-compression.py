e49306066fb974841117fd0287baaddfdf18815c
VANDIT TYAGI <91458535+vandit98@users.noreply.github.com>
2023-05-31 13:48:45 +0530

    Time: 73 ms (58.65%), Space: 16.6 MB (8.90%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -3,24 +3,43 @@ class Solution:
         tar=chars[0]
         count=0
         ans=""
+        j=0
         for i in range(len(chars)):
             if chars[i]==tar:
                 count+=1
                 # print(f"we got {chars[i]} and increasing count to ",count)
             else:
                 ans+=tar
+                chars[j]=tar
+                j+=1 
                 if count>1:
-                    ans+=f"{count}"
+                    if count>9:
+                        a=str(count)
+                        for k in a:
+                            chars[j]=k
+                            j+=1
+                        
+                    else:
+                        # ans+=f"{count}"
+                        chars[j]=f"{count}"
+                        j+=1
                 tar=chars[i]
                 count=1
-        ans+=tar
+        # ans+=tar
+        chars[j]=tar
+        j+=1
         if count>1:
-            ans+=f"{count}"
+            if count>9:
+                a=str(count)
+                for i in a:
+                    chars[j]=i
+                    j+=1
+            else:
+                # ans+=f"{count}"
+                chars[j]=f"{count}"
+                j+=1
         print(ans)
-        chars.clear()
-        for i in ans:
-            chars.append(i)
-        # chars=list(ans)
-        print("chars is ",chars)
+        # print("j is",j)
+        chars=chars[:j]
         return len(chars)
         
