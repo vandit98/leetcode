b6c3e6ea02abc9803c07921d79e13ce84feaf971
VANDIT TYAGI <91458535+vandit98@users.noreply.github.com>
2023-07-10 15:15:06 +0530

    Time: 46 ms (71.39%), Space: 16.3 MB (25.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -5,12 +5,9 @@ class Solution:
             return 0
         if n==1:
             return 1
-        arr=[-1]*(n+1)
-        arr[0]=0
-        arr[1]=1
+        arr=[0,1]
         for i in range(2,n+1):
-            arr[i]=arr[i-1]+arr[i-2]
-        print(arr)
-        return arr[n]
+            arr=[arr[1],arr[0]+arr[1]]
+        return arr[1]
             
             
