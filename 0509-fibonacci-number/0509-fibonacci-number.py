43db2cfd4afb9ab4855b6c0305f8d54f56470566
VANDIT TYAGI <91458535+vandit98@users.noreply.github.com>
2023-07-10 15:20:59 +0530

    Time: 46 ms (71.39%), Space: 16.4 MB (25.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,13 +1,10 @@
 class Solution:
-    
     def fib(self, n: int) -> int:
         if n==0:
             return 0
         if n==1:
             return 1
-        arr=[0,1]
+        arr=(0,1)
         for i in range(2,n+1):
-            arr=[arr[1],arr[0]+arr[1]]
+            arr=(arr[1],arr[0]+arr[1])
         return arr[1]
-            
-            
